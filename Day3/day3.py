106bfb41c53a16f3acb91745e79c9f86dd39f2fc
mdalzell <matt.a.dalzell@gmail.com>
2019-12-03 22:14:52 -0600

    Day 3

---
+++ Day3/day3.py
@@ -0,0 +1,72 @@
+import sys
+
+def calculateMinSignalDelay(wire1, wire2):
+    wire1pointsList = convertWireStringToPointSet(wire1)
+    wire2pointsList = convertWireStringToPointSet(wire2)
+    wire1points = set(wire1pointsList)
+    wire2points = set(wire2pointsList)
+    intersection = wire1points.intersection(wire2points)
+
+    minDistance = sys.maxsize
+    for point in intersection:
+        distance = wire1pointsList.index(point) + wire2pointsList.index(point) + 2
+        if distance < minDistance:
+            minDistance = distance
+
+    return minDistance
+
+def calculateManhattanDistance(wire1, wire2):
+    wire1pointsList = convertWireStringToPointSet(wire1)
+    wire2pointsList = convertWireStringToPointSet(wire2)
+    wire1points = set(wire1pointsList)
+    wire2points = set(wire2pointsList)
+    intersection = wire1points.intersection(wire2points)
+
+    minDistance = sys.maxsize
+    for point in intersection:
+        distance = abs(point[0]) + abs(point[1])
+        if distance < minDistance:
+            minDistance = distance
+
+    return minDistance
+
+def convertWireStringToPointSet(wire):
+    pointArray = []
+    currentCoordinate = (0, 0)
+    wireCoordinates = wire.split(',')
+
+    for wireCoordinate in wireCoordinates:
+        direction = wireCoordinate[0]
+        magnitude = int(wireCoordinate[1:])
+        if direction == 'R':
+            for x in range(currentCoordinate[0] + 1, currentCoordinate[0] + magnitude + 1):
+                currentCoordinate = (x, currentCoordinate[1])
+                pointArray.append(currentCoordinate)
+        elif direction == 'L':
+            for x in range(currentCoordinate[0] - 1, currentCoordinate[0] - magnitude - 1, -1):
+                currentCoordinate = (x, currentCoordinate[1])
+                pointArray.append(currentCoordinate)
+        elif direction == 'U':
+            for y in range(currentCoordinate[1] + 1, currentCoordinate[1] + magnitude + 1):
+                currentCoordinate = (currentCoordinate[0], y)
+                pointArray.append(currentCoordinate)
+        elif direction == 'D':
+            for y in range(currentCoordinate[1] - 1, currentCoordinate[1] - magnitude - 1, -1):
+                currentCoordinate = (currentCoordinate[0], y)
+                pointArray.append(currentCoordinate)
+
+    return pointArray
+
+if __name__ == '__main__':
+    wireStrings = []
+    with open("./input.txt") as input:
+        for line in input:
+            wireStrings.append(line)
+
+    if (len(sys.argv) > 1 and sys.argv[1] == "part2"):
+        result = calculateMinSignalDelay(wireStrings[0], wireStrings[1])
+        print(result)
+    else:
+        result = calculateManhattanDistance(wireStrings[0], wireStrings[1])
+        print(result)
+
---
+++ Day3/day3.test.py
@@ -0,0 +1,19 @@
+import unittest
+from day3 import calculateMinSignalDelay, calculateManhattanDistance
+
+class Day3Test(unittest.TestCase):
+
+    def test_calculateManhattanDistance_exampleCase1(self):
+        self.assertEqual(calculateManhattanDistance('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), 159)
+
+    def test_calculateManhattanDistance_exampleCase2(self):
+        self.assertEqual(calculateManhattanDistance('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 135)
+    
+    def test_calculateMinSignalDelay_exampleCase1(self):
+        self.assertEqual(calculateMinSignalDelay('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), 610)
+    
+    def test_calculateMinSignalDelay_exampleCase2(self):
+        self.assertEqual(calculateMinSignalDelay('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'), 410)
+
+if __name__ == '__main__':
+    unittest.main()
