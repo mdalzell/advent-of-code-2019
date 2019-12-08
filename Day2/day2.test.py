7c68fffdb085d090089772574516159a1a5842a3
mdalzell <matt.a.dalzell@gmail.com>
2019-12-07 18:34:14 -0600

    Fixing broken tests

--- Day2/day2.test.py
+++ Day2/day2.test.py
@@ -4,16 +4,16 @@ from day2 import intcode
 class Day2Test(unittest.TestCase):
 
     def test_intcode_exampleCase1(self):
-        self.assertEqual(intcode([1,0,0,0,99]), [2,0,0,0,99])
+        self.assertEqual(intcode([1,0,0,0,99], 0, 0), [2,0,0,0,99])
 
     def test_intcode_exampleCase2(self):
-        self.assertEqual(intcode([2,3,0,3,99]), [2,3,0,6,99])
+        self.assertEqual(intcode([2,3,0,3,99], 3, 0), [2,3,0,6,99])
 
     def test_intcode_exampleCase3(self):
-        self.assertEqual(intcode([2,4,4,5,99,0]), [2,4,4,5,99,9801])
+        self.assertEqual(intcode([2,4,4,5,99,0], 4, 4), [2,4,4,5,99,9801])
 
     def test_intcode_exampleCase4(self):
-        self.assertEqual(intcode([1,1,1,4,99,5,6,0,99]), [30,1,1,4,2,5,6,0,99])
+        self.assertEqual(intcode([1,1,1,4,99,5,6,0,99], 1, 1), [30,1,1,4,2,5,6,0,99])
 
 if __name__ == '__main__':
     unittest.main()
--- Day4/day4.test.py
+++ Day4/day4.test.py
@@ -1,17 +1,17 @@
 import unittest
 import re
-from day4 import exactlyTwoConsecutiveDigitsRegex, hasRegexMatch, validPassword
+from day4 import consecutiveDigitsRegex, exactlyTwoConsecutiveDigitsRegex, hasRegexMatch, validPassword
 
 class Day3Test(unittest.TestCase):
 
     def test_validPassword_exampleCase1(self):
-        self.assertEqual(validPassword(111111), True)
+        self.assertEqual(validPassword(111111, consecutiveDigitsRegex), True)
 
     def test_validPassword_exampleCase2(self):
-        self.assertEqual(validPassword(223450), False)
+        self.assertEqual(validPassword(223450, consecutiveDigitsRegex), False)
 
     def test_validPassword_exampleCase3(self):
-        self.assertEqual(validPassword(123789), False)
+        self.assertEqual(validPassword(123789, consecutiveDigitsRegex), False)
 
     def test_containsExactlyTwoConsecutiveSameDigits_exampleCase1(self):
         self.assertEqual(hasRegexMatch(112233, exactlyTwoConsecutiveDigitsRegex), True)
